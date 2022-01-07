bd5f0fadabfb065788029192ac40b6406dc69301
weiian000 <apple0911177679@gmail.com>
2022-01-07 11:16:10 +0800

    test linode

--- homework/final/web/app.js
+++ homework/final/web/app.js
@@ -1,32 +1,162 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import * as render from './render.js'
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
+import { Session } from "https://deno.land/x/oak_sessions/mod.ts";
 
-const posts = [
+const db = new DB("blog.db");
+db.query("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, title TEXT, body TEXT)");
 
-];
-
+db.query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, email TEXT)");
+/*
+const userMap = {
+  ccc: { username:'ccc', password: '123' },
+  snoopy: { username:'snoopy', password: '321' }
+}
+*/
+const session = new Session()
 const router = new Router();
 
-router.get('/', list)//列出所有貼文
-.get('/login', LoginUi)
-.get('/signup',signupUi)
+router.get('/',session.initMiddleware(), loginUi)
+  .post('/',session.initMiddleware(), login)
+  .get('/signup',session.initMiddleware(), signupUi)
+  .post('/signup',session.initMiddleware(), signup)
+  .get('/calendar',session.initMiddleware(),createcalendar)
+  .get('/list',session.initMiddleware(), list)
+  .get('/logout',session.initMiddleware(), logout)
+  .get('/post/new',session.initMiddleware(), add)
+  .get('/post/:id', session.initMiddleware(),show)
+  .post('/post',session.initMiddleware(), create)
+
 
 const app = new Application();
+
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-async function list(ctx) {
-  
-  ctx.response.body = await render.list(posts);
-}
-async function LoginUi(ctx) {
-  
-  ctx.response.body = await render.loginUi();
-}
-async function signupUi(ctx) {
-  
-  ctx.response.body = await render.signupUi();
+function sqlcmd(sql, arg1) { //參數為sql指令結果 和 參數
+  console.log('sql:', sql) //印出資料庫下的指令
+  try {
+    var results = db.query(sql, arg1)//從db裡將id username title body撈出來
+    console.log('sqlcmd: results=', results)
+    return results
+  } catch (error) {
+    console.log('sqlcmd error: ', error)
+    throw error
+  }
 }
 
-console.log('Server run at http://127.0.0.1:8000')
+function postQuery(sql) {//postQuery("SELECT id, username, title, body FROM posts")
+  let list = []
+  for (const [id, username, title, body] of sqlcmd(sql)) {//陣列順序和db創建欄位的順序有關
+    list.push({id, username, title, body})
+  }
+  console.log('postQuery: list=', list)
+  return list
+}
+
+function userQuery(sql) {//回傳user的資料庫內容
+  let list = []
+  for (const [id, username, password, email] of sqlcmd(sql)) {
+    list.push({id, username, password, email})
+  }
+  console.log('userQuery: list=', list)
+  return list
+}
+
+async function parseFormBody(body) {//把form的input的value轉為一個物件
+  const pairs = await body.value
+  const obj = {}
+  for (const [key, value] of pairs) {
+    obj[key] = value
+  }
+  return obj
+}
+
+async function signupUi(ctx) {
+  ctx.response.body = await render.signupUi();
+}
+async function createcalendar(ctx) {
+  ctx.response.body = await render.calendar();
+}
+async function signup(ctx) {
+  const body = ctx.request.body()
+  if (body.type === "form") {
+    var user = await parseFormBody(body)
+    var dbUsers = userQuery(`SELECT id, username, password, email FROM users WHERE username='${user.username}'`)//檢查帳號是否有人用依照username做判斷
+    if (dbUsers.length === 0) {
+      sqlcmd("INSERT INTO users (username, password, email) VALUES (?, ?, ?)", [user.username, user.password, user.email]);//沒有人用則將帳號匯入資料庫
+      ctx.response.body = render.success()
+    } else 
+      ctx.response.body = render.fail()
+  }
+}
+
+async function loginUi(ctx) {
+  ctx.response.body = await render.loginUi();
+}
+
+async function login(ctx) {
+  const body = ctx.request.body()
+  if (body.type === "form") {
+    var user = await parseFormBody(body)
+    var dbUsers = userQuery(`SELECT id, username, password, email FROM users WHERE username='${user.username}'`) // userMap[user.username]
+    var dbUser = dbUsers[0]
+    if (dbUser.password === user.password) {
+      ctx.state.session.set('user', user)
+      console.log('session.user=', await ctx.state.session.get('user'))
+      ctx.response.redirect('/list');
+    } else {
+      ctx.response.body = render.fail()
+    }
+  }
+}
+
+async function logout(ctx) {
+   ctx.state.session.set('user', null)
+   ctx.response.redirect('/')
+}
+
+async function list(ctx) {
+  let posts = postQuery("SELECT id, username, title, body FROM posts")
+  console.log('list:posts=', posts)
+  ctx.response.body = await render.list(posts, await ctx.state.session.get('user'));
+}
+
+async function add(ctx) {
+  var user = await ctx.state.session.get('user')
+  if (user != null) {
+    ctx.response.body = await render.newPost();
+  } else {
+    ctx.response.body = render.fail()
+  }
+}
+
+async function show(ctx) {
+  const pid = ctx.params.id;
+  let posts = postQuery(`SELECT id, username, title, body FROM posts WHERE id=${pid}`)
+  let post = posts[0]
+  console.log('show:post=', post)
+  if (!post) ctx.throw(404, 'invalid post id');
+  ctx.response.body = await render.show(post);
+}
+
+async function create(ctx) {
+  const body = ctx.request.body()
+  if (body.type === "form") {
+    var post = await parseFormBody(body)
+    console.log('create:post=', post)
+    var user = await ctx.state.session.get('user')
+    if (user != null) {
+      console.log('user=', user)
+      sqlcmd("INSERT INTO posts (username, title, body) VALUES (?, ?, ?)", [user.username, post.title, post.body]);  
+    } else {
+      ctx.throw(404, 'not login yet!');
+    }
+    ctx.response.redirect('/list');
+  }
+
+}
+
+console.log('Server run at http://127.0.0.1:8110')
 await app.listen({ port: 8110 });
--- homework/final/web/render.js
+++ homework/final/web/render.js
@@ -1,6 +1,6 @@
 
 
-export function layout(title,content){
+export function layout1(title,content){
     return `<html>
     <head>
         <meta charset="utf-8"/>
@@ -105,16 +105,81 @@ export function layout(title,content){
     <section id="content">
     ${content}
     </section>
-    <script src = "./connect.js"> </scirpt>
+    
     </body>
     </html> 
 `
 }
+export function layout(title, content) {
+    return `
+    <html>
+    <head>
+      <title>${title}</title>
+      <style>
+        body {
+          padding: 80px;
+          font: 16px Helvetica, Arial;
+        }
+    
+        h1 {
+          font-size: 2em;
+        }
+    
+        h2 {
+          font-size: 1.2em;
+        }
+    
+        #posts {
+          margin: 0;
+          padding: 0;
+        }
+    
+        #posts li {
+          margin: 40px 0;
+          padding: 0;
+          padding-bottom: 20px;
+          border-bottom: 1px solid #eee;
+          list-style: none;
+        }
+    
+        #posts li:last-child {
+          border-bottom: none;
+        }
+    
+        textarea {
+          width: 500px;
+          height: 300px;
+        }
+    
+        input[type=text],input[type=password],
+        textarea {
+          border: 1px solid #eee;
+          border-top-color: #ddd;
+          border-left-color: #ddd;
+          border-radius: 2px;
+          padding: 15px;
+          font-size: .8em;
+        }
+    
+        input[type=text],input[type=password] {
+          width: 500px;
+        }
+      </style>
+    </head>
+    <body>
+      <section id="content">
+        ${content}
+      </section>
+    </body>
+    </html>
+    `
+  }
 
 
 export function calendar(){
     let content = `
         <div class="title">
+            
             <h3 id="yearmonth" class = 'yearmonth'></h3>
             <button onclick="premonth();" class="pre">&#10094;</button>
             <button onclick="nextmonth();" class='next'>&#10095;</button>
@@ -134,6 +199,7 @@ export function calendar(){
             </div>
       </div>
         <div class = "list" id = "list"></div>
+        
         <script>
             //資料
             let state = null;
@@ -182,18 +248,20 @@ export function calendar(){
             }
             function renderDate(date,list)
             {
-                let cell = document.createElement("div");
+                let cell = document.createElement("a");
+                cell.setAttribute('href', '/post/new')
                 cell.className = "date" + (date.getMonth()===state.current.getMonth()?"":" fadeout");
                 cell.textContent =  date.getDate();
+            
                 list.appendChild(cell);//
             }
             //controller
             init();
-
         </script>
 </body>`
-return layout('行事曆',content)
+return layout1('行事曆',content)
 }
+
 export function list(posts,user){
     let list = []
     for(let post of posts){
@@ -201,13 +269,14 @@ export function list(posts,user){
         <li>
         <h2>${ post.title } -- by ${post.username}</h2>
         <p><a href="/post/${post.id}">Read post</a></p>
+        <p><a href="/list"> </a>delete post</p>
         </li>
         `
         )
     }
     let content = `
     <h1>Posts</h1>
-    <p><a href="/login">Login</a> to create your calendar</p>
+    <p>Welcome ${user.username}, You may <a href="/calendar">Create a Post</a> or <a href="/logout">Logout</a> !</p>
     <p>There are <strong>${posts.length}</strong> posts!</p>
     <ul id="posts">
     ${list.join('\n')}
@@ -219,10 +288,12 @@ export function list(posts,user){
 export function loginUi(){
     let content = `
     <h1>Login</h1>
-    <form action = "/login" method = "post">
+    <form action = "/" method = "post">
     <p><input type="text" placeholder="username" name="username"></p>
     <p><input type="password" placeholder="password" name="password"></p>
+    <p><input type="text" placeholder="email" name="email"></p>
     <p><input type="submit" value="Login"></p>
+
     <p>New user? <a href="/signup">Create an account</p>
     </form>
     `
@@ -241,9 +312,34 @@ export function signupUi(){
     return layout('Signup',content)
 }
 
+export function success() {
+    return layout('Success', `
+    <h1>Success!</h1>
+    You can <a href="/">login now !</a> 
+    `)
+  }
+  
+  export function fail() {
+    return layout('Fail', `
+    <h1>Fail!</h1>
+    Your username had been use by other people . Please <a href="JavaScript:window.history.back()">Signup again</a> !
+    `)
+  }
+
 export function newPost(){
-
+    return layout('New Post', `
+    <h1>New Post</h1>
+    <p>Create a new post.</p>
+    <form action="/post" method="post">
+      <p><input type="text" placeholder="Title" name="title"></p>
+      <p><textarea placeholder="Contents" name="body"></textarea></p>
+      <p><input type="submit" value="Create"></p>
+    </form>
+    `)
 }
-export function show(){
-
+export function show(post){
+    return layout(post.title, `
+    <h1>${post.title} -- by ${post.username}</h1>
+    <p>${post.body}</p>
+  `)
 }
